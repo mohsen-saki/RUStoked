e7a461afb6ddcb26aaef0b50bf2e433f85f9b5e4
mohsen-saki <saki.mohsen@gmail.com>
2020-08-09 10:03:20 +1000

    add github workflow for deploying docker contaner to heroku

--- app/rustoked.py
+++ app/rustoked.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import joblib
 import pandas as pd
+import streamlit as st
 
 current_path = Path(os.path.dirname(__file__))
 
@@ -11,6 +12,7 @@ vectorizer_path = Path("../models/vectorizer_v1.pkl")
 VECTORIZER = joblib.load(current_path / vectorizer_path)
 MODEL = joblib.load(current_path / model_path)
 
+@st.cache(suppress_st_warning=True)
 def get_prediction(text):
     sentiment = ["Dissatisfied", "Disengaged", "Stoked"]
     df = pd.DataFrame([text], columns=["comment"])
